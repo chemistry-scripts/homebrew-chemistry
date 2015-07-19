d7a552d1e380744df237ac92b8b684415c362215
Emmanuel Nicolas <emmanuel.nicolas_svn@polytechnique.org>
2015-07-19 22:17:53 +0200

    First Default Entry for platon. Not working

---
+++ platon.rb
@@ -0,0 +1,33 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Platon < Formula
+  desc ""
+  homepage ""
+  url "http://www.platonsoft.nl/xraysoft/unix/platon/platon.f.gz"
+  sha256 ""
+  version "30715"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # Download additional library
+
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test platon`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
